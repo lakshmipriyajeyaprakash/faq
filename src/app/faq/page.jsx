b339960e3be78c39b27834a1fbf3ee758650c0c4
lakshmijeybe97@gmail.com <lakshmijeybe97@gmail.com>
2024-08-27 16:40:54 -0400

    setting search url logic

--- src/app/faq/page.jsx
+++ src/app/faq/page.jsx
@@ -4,44 +4,36 @@ import faqInput from '../faqData/faqInput';
 import FaqsItem from './_components/FaqsItem';
 import { FaChevronUp, FaChevronDown } from "react-icons/fa";
 import { FaArrowRight } from "react-icons/fa6";
+import { useRouter } from 'next/navigation';
+import { useSearchParams } from 'next/navigation';
 
 const faq = () => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
   const [openIndexes, setOpenIndexes] = useState([]);
   const [toggleInd, setToggleInd] = useState(false);
-  const [open, setOpen] = useState(false);
-  const [inputVal, setInputVal] = useState('');
-
+  const [open, setOpen] = useState(null);
+  const [inputVal, setInputVal] = useState(!!searchParams.get("search") ? searchParams.get("search") : '');
+  
   const findWords = (e) => {
     e.preventDefault();
     setInputVal(e.target.value);
   };
-
-  const highlightSearch = (faqText) => {
-    if (!inputVal) 
-      return faqText;
-      const splitSearch = new RegExp(`(${inputVal})`, 'gi');
-      return faqText.split(splitSearch).map((splitted, index) =>
-        splitted.toLowerCase() === inputVal.toLowerCase()?(<span key={index} className="bg-blue-200">{splitted}</span>):(splitted)
-      
-    );
-  };
+ 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    router.push(`/faq?search=${inputVal}`);
+  }
 
   const toggle = (index) => {
-    if (toggleInd === true) {
+    if (toggleInd) {
       if (openIndexes.includes(index)) {
         setOpenIndexes(openIndexes.filter((i) => i !== index));
-      } else if (toggleInd && openIndexes.length > 0) {
-        setOpen(index);
-        setOpenIndexes([]);
       } else {
-        setOpenIndexes([...openIndexes, index]);
+        setOpenIndexes([index]);
       }
     } else {
-      if (index === open) {
-        setOpen(null);
-      } else {
-        setOpen(index);
-      }
+      setOpen(open === index ? null : index);
     }
   };
 
@@ -55,13 +47,22 @@ const faq = () => {
     }
   };
 
+  // FAQs Filter based on the search input
+  const filteredFaqs = faqInput.filter(faqans =>
+    faqans.question.toLowerCase().includes(inputVal.toLowerCase())
+  );
+
+  // Display FAQ
+  const faqsToDisplay = filteredFaqs.length > 0 ? filteredFaqs : faqInput;
+
   return (
     <div className="w-full h-screen">
-      <h1 className="px-5 flex justify-center py-10 flex-wrap font-bold text-slate-900">
+      <h1 className="px-5 flex justify-center py-10 flex-wrap text-slate-900 font-extrabold">
         Frequently Asked Questions
       </h1>
       <div className="flex items-center justify-center cursor-pointer">
         <div className="relative w-60">
+          <form onSubmit={handleSubmit}>
           <input 
             type="text" 
             className="w-full px-4 py-2 rounded-full box-border border border-gray-200 focus:shadow-md transition-all duration-300 delay-150" 
@@ -69,9 +70,10 @@ const faq = () => {
             value={inputVal} 
             onChange={findWords}
           />
-          <button className="absolute right-0 top-0 mt-1 mr-2 bg-gray-500 text-white p-2 rounded-full focus:outline-none">
+          <button type='submit' className="absolute right-0 top-0 mt-1 mr-2 bg-gray-500 text-white p-2 rounded-full focus:outline-none">
             <FaArrowRight />
-          </button>
+            </button>
+            </form>
         </div>
       </div>
       <div className="px-5 py-5 flex justify-center">
@@ -90,16 +92,28 @@ const faq = () => {
               )}
             </button>
           </div>
-          <div className="h-max border rounded-lg overflow-hidden hover:cursor-pointer">
-            {faqInput.map((faqans, index) => (
-              <FaqsItem
-                key={index}
-                openAll={openIndexes.includes(faqans.id)}
-                open={open === faqans.id}
-                toggle={() => toggle(faqans.id)}
-                question={highlightSearch(faqans.question)}
-                answer={faqans.answer}
-              />
+          <div className="h-max border rounded-lg overflow-hidden ">
+            {faqsToDisplay.map((faqans, index) => (
+              <div key={index} className="border-b last:border-b-0">
+              <div
+                className={`px-5 py-4 flex items-center justify-between hover:cursor-pointer ${
+                  open === faqans.id || openIndexes.includes(faqans.id) ? "bg-gray-100 shadow-md" : ""
+                }`}
+                onClick={() => toggle(faqans.id)}
+              >
+                <p className="flex-1">{faqans.question}</p>
+                <span>{open === faqans.id || openIndexes.includes(faqans.id) ? <FaChevronUp /> : <FaChevronDown />}</span>
+              </div>
+              <div className="px-5 flex items-center justify-between">
+                {open === faqans.id || openIndexes.includes(faqans.id) ? (
+                  <div className="">
+                    <p className="flex-1 py-4 ">{faqans.answer}</p>
+                  </div>
+                ) : (
+                  ""
+                )}
+              </div>
+            </div>
             ))}
           </div>
         </div>
