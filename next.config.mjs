d81a79e89e3ee9a7bd8051def8ee39a783d9c605
lakshmijeybe97@gmail.com <lakshmijeybe97@gmail.com>
2024-09-19 16:56:41 -0400

    Reverted to Initial configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/", // Only redirect root
+        destination: "/faq",
+        permanent: true, // Only if it's a permanent redirect
+      },
+    ];
+  },
+};
 
 export default nextConfig;
--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,4 @@
 import faq from "./faq/page";
 export default function Home() {
-  return (
-    <div>
-      <faq></faq>
-    </div>
-  );
+  return <div>Happy Coding</div>;
 }
